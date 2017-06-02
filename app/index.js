2c419a8bcdf71c3a50e16b99be1aa969ec703a55
Nedu <nedumrobert@gmail.com>
2017-06-02 15:46:35 +0100

    Updates

--- app/index.js
+++
@@ -1,6 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var WikipediaViewer = require('./components/WikipediaViewer');
-require('./index.css');
-
-ReactDOM.render(<WikipediaViewer />, document.getElementById('wikipedia-viewer'));
